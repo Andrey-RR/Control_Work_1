2f875c62130aed1f96d5c0cb4f5d56c8f56bd1ed
andrey <13rred@gmail.com>
2023-12-16 00:32:47 +0300

    update task (Prompt , FillArray

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int length = Prompt ("Введите количество элементов массива : ");
+int numbsymbol = Prompt ("Введите предельное количество символов : ");
+string[] stringarray;
+stringarray = FillArray(length);
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // вывести сообщение
+    string value =Console.ReadLine(); // считывает с консоли строку
+    int result = Convert.ToInt32(value); // преобразует строку в целое число
+    return result;
+}
+
+string[] FillArray (int length)
+{
+    string[] stringarray = new string[length];
+    for(int i = 0; i < stringarray.Length; i++)
+        {
+        Console.WriteLine($"Введите элемент массива под индексом {i}:");
+        stringarray[i] = Console.ReadLine();
+        }
+    return stringarray;
+}
